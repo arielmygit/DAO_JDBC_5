13f232508696ff87b3597c8ec0d6cf673995e99b
arielmygit <126427169+arielmygit@users.noreply.github.com>
2023-05-20 22:44:23 -0600

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,26 @@
+package org.example;
+
+import org.example.modelo.Libro;
+import org.example.persistencia.DemoLibroDB;
+import org.example.persistencia.LibroDAO;
+
+import java.sql.SQLException;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        LibroDAO ldao = new LibroDAO();
+        Libro libro = new Libro(1,"El perfume","Patrick Suskind");
+
+        try {
+           Libro res= (Libro)ldao.buscarPorId("1");
+            System.out.println(res);
+            System.out.println("-----------");
+            for (Object lib : ldao.obtenerTodo()) {
+                System.out.println((Libro)lib);
+            }
+
+        } catch (SQLException sqle){
+        System.out.println("Error al editar");}
+    }
+}
